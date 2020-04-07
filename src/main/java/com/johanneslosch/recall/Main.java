b5936afae85c9cb01909bbddf2a58d82453ae120
Johannes Losch <hi@johanneslosch.com>
2020-04-07 22:01:42 +0200

    #1 added Telegram bot
    @re_call_bot on telegram

---
+++ src/main/java/com/johanneslosch/recall/Main.java
@@ -0,0 +1,17 @@
+package com.johanneslosch.recall;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Recall());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
