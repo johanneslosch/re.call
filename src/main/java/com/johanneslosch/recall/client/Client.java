cddf7dcea86d95b8fd7c5dd126e2df95f9d44c98
Restyled.io <commits@restyled.io>
2020-04-14 19:06:55 +0000

    Restyled by clang-format

--- src/main/java/com/johanneslosch/recall/client/Client.java
+++ src/main/java/com/johanneslosch/recall/client/Client.java
@@ -1,28 +1,27 @@
 package com.johanneslosch.recall.client;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.java_websocket.client.WebSocketClient;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class Client {
-    static WebSocketClient client;
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(new Recall());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-        try {
-            client = new SocketClient(new URI("ws://localhost:8887"));
-            client.connect();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+  static WebSocketClient client;
+  public static void main(String[] args) {
+    ApiContextInitializer.init();
+    TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+    try {
+      telegramBotsApi.registerBot(new Recall());
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
     }
+    try {
+      client = new SocketClient(new URI("ws://localhost:8887"));
+      client.connect();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/com/johanneslosch/recall/client/Recall.java
+++ src/main/java/com/johanneslosch/recall/client/Recall.java
@@ -4,25 +4,31 @@ import com.johanneslosch.recall.util.ConfigReader;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class Recall  extends TelegramLongPollingBot {
-    @Override
-    public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            //message content
-            update.getMessage().getText();
-            System.out.println(String.format("RECEIVED: message: %s -> at: %s -> from: %s", update.getMessage().getText(), System.nanoTime(), update.getMessage().getChat().getUserName()));
-            Client.client.send(String.format("RECEIVED: message: %s -> at: %s -> from: %s", update.getMessage().getText(), System.nanoTime(), update.getMessage().getChat().getUserName()));
-        }
+public class Recall extends TelegramLongPollingBot {
+  @Override
+  public void onUpdateReceived(Update update) {
+    // We check if the update has a message and the message has text
+    if (update.hasMessage() && update.getMessage().hasText()) {
+      // message content
+      update.getMessage().getText();
+      System.out.println(
+          String.format("RECEIVED: message: %s -> at: %s -> from: %s",
+                        update.getMessage().getText(), System.nanoTime(),
+                        update.getMessage().getChat().getUserName()));
+      Client.client.send(
+          String.format("RECEIVED: message: %s -> at: %s -> from: %s",
+                        update.getMessage().getText(), System.nanoTime(),
+                        update.getMessage().getChat().getUserName()));
     }
+  }
 
-    @Override
-    public String getBotUsername() {
-        return "re_call_bot";
-    }
+  @Override
+  public String getBotUsername() {
+    return "re_call_bot";
+  }
 
-    @Override
-    public String getBotToken() {
-        return ConfigReader.read("data", "config", "BotToken");
-    }
+  @Override
+  public String getBotToken() {
+    return ConfigReader.read("data", "config", "BotToken");
+  }
 }
