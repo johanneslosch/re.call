44f4fdd15ca618f570dc201acce1d0487a193494
Johannes Losch <hi@johanneslosch.com>
2020-04-14 21:03:52 +0200

    added WebsocketClient

---
+++ src/main/java/com/johanneslosch/recall/client/SocketClient.java
@@ -0,0 +1,45 @@
+package com.johanneslosch.recall.client;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.nio.ByteBuffer;
+
+public class SocketClient extends WebSocketClient {
+
+    public SocketClient(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public SocketClient(URI serverURI) {
+        super(serverURI);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        send(String.format("Connect from %s", handshakedata.getHttpStatus()));
+        System.out.println("new connection opened");
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("closed with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("received message: " + message);
+    }
+
+    @Override
+    public void onMessage(ByteBuffer message) {
+        System.out.println("received ByteBuffer");
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.err.println("an error occurred:" + ex);
+    }
+}
