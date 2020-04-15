98a8af1ebee131ea3a8e536a83f73aff5a2d9d73
Restyled.io <commits@restyled.io>
2020-04-15 17:34:33 +0000

    Restyled by clang-format

--- src/main/java/com/johanneslosch/recall/server/HandleMessages.java
+++ src/main/java/com/johanneslosch/recall/server/HandleMessages.java
@@ -1,6 +1,5 @@
 package com.johanneslosch.recall.server;
 
-
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,69 +7,71 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class HandleMessages {
-    public static void message(String message) {
-        if (message.contains(";")) {
-            try {
-                handleMessage(message);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
+  public static void message(String message) {
+    if (message.contains(";")) {
+      try {
+        handleMessage(message);
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    private static void handleMessage(String message) throws ParseException {
-        String[] part = message.split(";", 25500);
-        String user = message.substring( message.indexOf("-> from:")).replace("-> from:", "");
-        String msg = part[0];
-        String date = part[1];
-        String time = part[2].replace("-> from:", "").replace(user, "");
-        System.out.println(String.format("-> %s at: %s: %s / %s", msg, date, time, user));
+  private static void handleMessage(String message) throws ParseException {
+    String[] part = message.split(";", 25500);
+    String user =
+        message.substring(message.indexOf("-> from:")).replace("-> from:", "");
+    String msg = part[0];
+    String date = part[1];
+    String time = part[2].replace("-> from:", "").replace(user, "");
+    System.out.println(
+        String.format("-> %s at: %s: %s / %s", msg, date, time, user));
 
-        System.out.println(String.format("message: %s by %s time: %sdate: %s", msg, user, time, handleDate(date)));
-        try {
-            MySQL.MySQLUseDataManager.setData(message, user, handleDate(date), time);
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-        }
+    System.out.println(String.format("message: %s by %s time: %sdate: %s", msg,
+                                     user, time, handleDate(date)));
+    try {
+      MySQL.MySQLUseDataManager.setData(message, user, handleDate(date), time);
+    } catch (SQLException throwable) {
+      throwable.printStackTrace();
     }
+  }
 
-    /*
-    - td - today;
-    - tm - tomorrow)
-    - nw - next week (in 7 days
-    - nm - next month (in 25 days))
-     */
-    private static String handleDate(String date) throws ParseException {
-        String finalDate = null;
-        if(date.equalsIgnoreCase("td")){
-            finalDate = getDate(0);
-        }else if(date.equalsIgnoreCase("tm")){
-            finalDate = getDate(1);
-        }else if(date.equalsIgnoreCase("nw")){
-            finalDate = getDate(7);
-        }else if(date.equalsIgnoreCase("nm")){
-            finalDate = getDate(25);
-        }else{
-               finalDate = date;
-        }
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
-        return finalDate;
+  /*
+  - td - today;
+  - tm - tomorrow)
+  - nw - next week (in 7 days
+  - nm - next month (in 25 days))
+   */
+  private static String handleDate(String date) throws ParseException {
+    String finalDate = null;
+    if (date.equalsIgnoreCase("td")) {
+      finalDate = getDate(0);
+    } else if (date.equalsIgnoreCase("tm")) {
+      finalDate = getDate(1);
+    } else if (date.equalsIgnoreCase("nw")) {
+      finalDate = getDate(7);
+    } else if (date.equalsIgnoreCase("nm")) {
+      finalDate = getDate(25);
+    } else {
+      finalDate = date;
     }
+    SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+    return finalDate;
+  }
 
-    //return Date (in days) in dd.MM.yyyy
-    private static String getDate(int days) throws ParseException {
-        Date dt = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(dt);
-        c.add(Calendar.DATE, days);
-        dt = c.getTime();
+  // return Date (in days) in dd.MM.yyyy
+  private static String getDate(int days) throws ParseException {
+    Date dt = new Date();
+    Calendar c = Calendar.getInstance();
+    c.setTime(dt);
+    c.add(Calendar.DATE, days);
+    dt = c.getTime();
 
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    String date = sdf.format(c.getTime());
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String date = sdf.format(c.getTime());
+    System.out.println(date);
 
-        System.out.println(date);
-
-        return date;
-    }
+    return date;
+  }
 }
--- src/main/java/com/johanneslosch/recall/server/MySQL.java
+++ src/main/java/com/johanneslosch/recall/server/MySQL.java
@@ -1,7 +1,6 @@
 package com.johanneslosch.recall.server;
 
 import com.johanneslosch.recall.util.ConfigReader;
-
 import java.sql.*;
 
 /*
@@ -9,85 +8,95 @@ import java.sql.*;
  */
 
 public class MySQL {
-    private static final String PORT = ConfigReader.read("data", "config", "dbPort");
-    private static final String DBNAME = ConfigReader.read("data", "config", "dbName");
-    private static final String USER = ConfigReader.read("data", "config", "dbUser");
-    private static final String PASSWORD = ConfigReader.read("data", "config", "dbPassword");
+  private static final String PORT =
+      ConfigReader.read("data", "config", "dbPort");
+  private static final String DBNAME =
+      ConfigReader.read("data", "config", "dbName");
+  private static final String USER =
+      ConfigReader.read("data", "config", "dbUser");
+  private static final String PASSWORD =
+      ConfigReader.read("data", "config", "dbPassword");
 
-    private static final String DATABASE_URL = "jdbc:mysql://" + ConfigReader.read("data", "config", "dbHost") + ":" + PORT + "/" + DBNAME;
-    private static Connection connection;
-    private static PreparedStatement prepareStatement;
+  private static final String DATABASE_URL =
+      "jdbc:mysql://" + ConfigReader.read("data", "config", "dbHost") + ":" +
+      PORT + "/" + DBNAME;
+  private static Connection connection;
+  private static PreparedStatement prepareStatement;
 
-    /**
-     * @return the connection
-     */
-    // connect database
-    private static Connection connect() {
-        if (connection == null) {
-            System.out.println("Connecting database...");
+  /**
+   * @return the connection
+   */
+  // connect database
+  private static Connection connect() {
+    if (connection == null) {
+      System.out.println("Connecting database...");
 
-            try {
-                connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-                System.out.println("Database connected!");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return connection;
+      try {
+        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+        System.out.println("Database connected!");
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
     }
-    private static void disconnect(){
-        if(connection != null){
-            try {
-                connection.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            System.out.println("Database Disconnected");
-        }
+    return connection;
+  }
+  private static void disconnect() {
+    if (connection != null) {
+      try {
+        connection.close();
+      } catch (SQLException throwables) {
+        throwables.printStackTrace();
+      }
+      System.out.println("Database Disconnected");
     }
-    private static PreparedStatement preparedStatementStorage(String message, String sender, String date, String time) throws SQLException {
-        connect();
-        prepareStatement = connection.prepareStatement(String.format("INSERT INTO `remember`(`sender`, `date`, `time`, `message`) VALUES ( \"%s\",\"%s\",\"%s\", \"%s\")", sender, date, time, message));
-        prepareStatement.execute();//
-        return prepareStatement;
+  }
+  private static PreparedStatement
+  preparedStatementStorage(String message, String sender, String date,
+                           String time) throws SQLException {
+    connect();
+    prepareStatement = connection.prepareStatement(String.format(
+        "INSERT INTO `remember`(`sender`, `date`, `time`, `message`) VALUES ( \"%s\",\"%s\",\"%s\", \"%s\")",
+        sender, date, time, message));
+    prepareStatement.execute(); //
+    return prepareStatement;
+  }
+
+  //    static PreparedStatement deleteData(int id, String table) throws
+  //    SQLException {
+  //        connect();
+  //        prepareStatement = connection.prepareStatement("DELETE FROM " +
+  //        table + " WHERE `id` = " + id + ")"); prepareStatement.execute();//
+  //        return prepareStatement;
+  //    }
+
+  /**
+   * Helps to get data from DataBase
+   *
+   * @param category category in DataBase
+   * @param table    a mySQL table
+   * @return a resultSet for usage
+   * @throws SQLException throws in Error
+   */
+  private static ResultSet getData(String category, String table)
+      throws SQLException {
+    connect();
+    Statement statement = null;
+    statement = connection.createStatement();
+    String sql = ("SELECT " + category + " FROM `" + table + "`;");
+    assert statement != null;
+
+    ResultSet resultSet = statement.executeQuery(sql);
+    resultSet.next();
+    return resultSet;
+  }
+
+  /**
+   * Class to get Data from Database
+   */
+  public static class MySQLUseDataManager extends MySQL {
+    public static void setData(String message, String sender, String date,
+                               String time) throws SQLException {
+      MySQL.preparedStatementStorage(message, sender, date, time);
     }
-
-
-//    static PreparedStatement deleteData(int id, String table) throws SQLException {
-//        connect();
-//        prepareStatement = connection.prepareStatement("DELETE FROM " + table + " WHERE `id` = " + id + ")");
-//        prepareStatement.execute();//
-//        return prepareStatement;
-//    }
-
-    /**
-     * Helps to get data from DataBase
-     *
-     * @param category category in DataBase
-     * @param table    a mySQL table
-     * @return a resultSet for usage
-     * @throws SQLException throws in Error
-     */
-    private static ResultSet getData(String category, String table) throws SQLException {
-        connect();
-        Statement statement = null;
-        statement = connection.createStatement();
-        String sql = ("SELECT " + category + " FROM `" + table + "`;");
-        assert statement != null;
-
-
-        ResultSet resultSet = statement.executeQuery(sql);
-        resultSet.next();
-        return resultSet;
-    }
-
-
-    /**
-     * Class to get Data from Database
-     */
-    public static class MySQLUseDataManager extends MySQL {
-        public static void setData(String message, String sender, String date, String time) throws SQLException {
-            MySQL.preparedStatementStorage(message, sender, date, time);
-        }
-    }
+  }
 }
